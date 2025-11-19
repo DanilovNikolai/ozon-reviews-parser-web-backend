f3e126d41d4382fa028c3f5e4bf5586eacaecc4e
Danilov_Nikolai <dan1k91@mail.ru>
2025-11-19 10:26:18 +0300

    feat: add local time

--- utils/getFormattedTimestamp.js
+++ utils/getFormattedTimestamp.js
@@ -1,11 +1,16 @@
 function getFormattedTimestamp() {
   const now = new Date();
+
+  // Москва +3 часа
+  const local = new Date(now.getTime() + 3 * 60 * 60 * 1000);
+
   const pad = (n) => String(n).padStart(2, '0');
-  const day = pad(now.getDate());
-  const month = pad(now.getMonth() + 1);
-  const year = String(now.getFullYear()).slice(2);
-  const hours = pad(now.getHours());
-  const minutes = pad(now.getMinutes());
+  const day = pad(local.getDate());
+  const month = pad(local.getMonth() + 1);
+  const year = String(local.getFullYear()).slice(2);
+  const hours = pad(local.getHours());
+  const minutes = pad(local.getMinutes());
+
   return `[${day}.${month}.${year} | ${hours}:${minutes}]`;
 }
 
