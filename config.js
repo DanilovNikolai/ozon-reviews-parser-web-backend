34758a8c2a7bee2699c6cf92b46d22f7353d8929
Danilov_Nikolai <dan1k91@mail.ru>
2025-12-01 13:25:26 +0300

    refactor: transfer screenshot variables from main.js

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ const CONFIG = {
   maxScrollAttempts: 50,
   nextPageTimeout: 30000,
   maxPagesPerSKU: 400,
+  firstScreenshotPath: '/tmp/debug_hash.png',
+  lastScreenshotPath: '/tmp/debug_reviews.png',
 };
 
 module.exports = { CONFIG };
